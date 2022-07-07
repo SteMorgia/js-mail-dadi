b796d3af01046f9b770a9d9cf3243d556fe87697
Stefano Morgia <ste.morgia@gmail.com>
2022-07-07 14:57:27 +0200

    creazione cartelle + prove esercizio mail

---
+++ mail/js/main.js
@@ -0,0 +1,8 @@
+let email = (prompt('Inserisci la tua email'));
+console.log(email);
+const indirizzo = ['ste.morgia@gmail.com', 'ciao@gmail.com', 'pluto93@gmail.com', 'jacopotoptutor@libero.it', 'alessiobosstutor@hotmail.it'];
+if (email = indirizzo) {
+    alert('La mail inserita è corretta')
+} else {
+    alert('Non sei invitato alla mensa del Signore')
+}
