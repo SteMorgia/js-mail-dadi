63f512a36824b02606f9235820439bef5cb8cf40
Stefano Morgia <ste.morgia@gmail.com>
2022-07-07 15:23:37 +0200

    prove js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,8 +1,12 @@
 let email = (prompt('Inserisci la tua email'));
 console.log(email);
 const indirizzo = ['ste.morgia@gmail.com', 'ciao@gmail.com', 'pluto93@gmail.com', 'jacopotoptutor@libero.it', 'alessiobosstutor@hotmail.it'];
-if (email = indirizzo) {
-    alert('La mail inserita è corretta')
-} else {
-    alert('Non sei invitato alla mensa del Signore')
+console.log(indirizzo[0]);
+for ( let i = 0; i < indirizzo.length; i++) {
+    console.log(indirizzo[i]);
+    if (email == i) {
+        alert('La mail inserita è corretta')
+    } else {
+        alert('Non sei invitato alla mensa del Signore')
+    }
 }
